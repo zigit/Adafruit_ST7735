236f5abfd7ef61e2cf958b2cbd18eb0c2955911a
Menan Vadivel <menan.vadivel@gmail.com>
2015-03-04 22:57:21 -0500

    header added

--- firmware/Adafruit_mfGFX.cpp
+++ firmware/Adafruit_mfGFX.cpp
@@ -38,7 +38,7 @@ POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include "Adafruit_mfGFX.h"
-#include "../fonts.h"
+#include "fonts.h"
 
 
 #define pgm_read_byte(addr) (*(const uint8_t *)(addr))
--- firmware/Adafruit_mfGFX.h
+++ firmware/Adafruit_mfGFX.h
@@ -2,7 +2,7 @@
 #define _ADAFRUIT_GFX_H
 
 #include "application.h"
-#include "../fonts.h"
+#include "fonts.h"
 
 
 #define swap(a, b) { int16_t t = a; a = b; b = t; }
